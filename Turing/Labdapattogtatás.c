d450e8e4662457a44ab19df7416a8c7c5f544e5f
CitromLovacska <45716160+CitromLovacska@users.noreply.github.com>
2019-03-03 18:50:36 +0100

    Update Labdapattogtatás.c

--- Turing/Labdapattogtatás.c
+++ Turing/Labdapattogtatás.c
@@ -1,54 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <curses.h>
 #include <unistd.h>
 
 int
 main ( void )
 {
-    WINDOW *ablak;
-    ablak = initscr ();
+	WINDOW *ablak;
+	ablak = initscr ();
 
-    int x = 0;
-    int y = 0;
+	int x = 0;
+	int y = 0;
 
-    int xnov = 1;
-    int ynov = 1;
+	int xnov = 1;
+	int ynov = 1;
 
-    int mx;
-    int my;
+	int mx;
+	int my;
 
-    for ( ; ; )
+	for ( ; ; )
 	{
-        getmaxyx ( ablak, my , mx );
+		getmaxyx ( ablak, my , mx );
 	
-        mvprintw ( y, x, "O" );
+		mvprintw ( y, x, "O" );
 	
-        refresh ();
+		refresh ();
 	
-        usleep ( 100000 );
+		usleep ( 100000 );
 
-        x = x + xnov;
-        y = y + ynov;
+		x = x + xnov;
+		y = y + ynov;
 
-        if ( x>=mx-1 )
-	{ 
-	xnov = xnov * -1;
-        }
-	
-        if ( x<=0 )
-	{
-	xnov = xnov * -1;
-        }
-	
-        if ( y<=0 )
-	{
-	xnov = ynov * -1;
-        }
-	
-        if ( y>=my-1 )
-	{
-	ynov = ynov * -1;
-        }
-    }
-    return 0;
+		if ( x>=mx-1 )
+		{
+			xnov = xnov * -1;
+		}
+
+		if ( x<=0 )
+		{
+			xnov = xnov * -1;
+		}
+
+		if ( y<=0 )
+		{
+			xnov = ynov * -1;
+		}
+
+		if ( y>=my-1 )
+		{
+			ynov = ynov * -1;
+		}
+	}
+	return 0;
 }
